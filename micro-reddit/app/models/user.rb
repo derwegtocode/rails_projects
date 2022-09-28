b50ef0917e1b445464b2cedc1abdbcadb608d84e
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-09-28 19:37:19 +0200

    modelo de user y post creado, validado y asociado.

---
+++ micro-reddit/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { maximum: 15, minimum: 5 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { maximum: 15, minimum: 5 }
+
+  has_many :post
+end
