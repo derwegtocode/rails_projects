fbf0e8961c3de1488b6ecd0a75be9fad69260612
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-10-03 19:41:03 +0200

    app funcional.

--- members-only/app/controllers/posts_controller.rb
+++ members-only/app/controllers/posts_controller.rb
@@ -5,16 +5,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.post.build(post_params.merge(user_id: current_user.email(/^[^@]*/)))
+    @post = current_user.post.build(post_params.merge(user_id: current_user))
     if @post.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Secret shared"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def index
-    @posts = Post.all.order(created_at: :asc)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   private
