a6f752fc89050ce677ae7b9a559bb2e2fe4b7291
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-10-06 01:46:02 +0200

    home controller creado

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "posts#index"
+  root to: "home#index"
 
   get "show", to: "posts#show"
 
